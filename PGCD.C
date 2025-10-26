dbaf2f850b294d76d70ffedb06b7c52fdc145909
intissartnaibi-glitch <intissar.tnaibi@uit.ac.ma>
2025-10-26 17:13:52 +0100

    Update PGCD.C

--- PGCD.C
+++ PGCD.C
@@ -1,10 +1,24 @@
  
  Tp_PGCD
 ♡ 1.définition : le PGCD de deux nombres est le plus grand nombre qui divise les deux sans reste .
+   _____
+       ♡ 2. algorithme d’euclide :
 
-♡ 2. algorithme d’euclide : algorithme algorithme d’euclide variable a,b ,PGCD:entier début Ecrire("Entrez deux nombres entiers") lire (a ,b) Tant que b≠0 r recoit a mod b a recoit b b recoit r Écrire ("le PGCD est : ",a) fin
 
-♡ 3 .exemples :
+ algorithme d’euclide 
+variable a,b ,PGCD:entier
+ début
+ Ecrire("Entrez deux nombres entiers")
+ lire (a ,b) 
+Tant que b≠0
+ r recoit a mod b 
+a recoit b
+ b recoit r
+ Écrire ("le PGCD est : ",a) 
+fin
+
+    _______
+                     ♡ 3 .exemples :
 
 a=90 ,b=60
 90÷60=1 est reste est 30 60÷30=2 est le reste =0 ■ PGCD de 90÷60 est 30
@@ -14,8 +28,10 @@ a=40,b=36
 
 a=100 , b=32
 100÷32=3 est le reste =4 32÷4=8 est le rest =0 ■ PGCD de 100÷32 est 4
+
+
 ●●●●
-4. la programmation de PGCD par language c:
+          4. la programmation de PGCD par language c:
 
 #include <stdio.h>
 
